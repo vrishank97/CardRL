76897b7b7d84fbeabe55bcdf05b4695ed61d5c83
Vrishank Bhardwaj <vrishank1997@gmail.com>
2018-08-15 22:34:06 +0530

    Create texas.py

---
+++ cardrl/games/texas.py
@@ -0,0 +1,15 @@
+class Texas:
+	def __init__(self, num_agents, buy_in):
+		"""
+		Used to set environment variables
+		"""
+
+	def step(self, move):
+		"""
+		Player uses this function to make a move
+		"""
+
+	def reset(self):
+		"""
+		Resets all game variables
+		"""
