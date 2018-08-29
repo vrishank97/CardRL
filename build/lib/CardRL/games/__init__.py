969c677415dfdc6cde4e09578441e7929e3a7f10
Vrishank Bhardwaj <vrishank1997@gmail.com>
2018-08-29 23:58:55 +0530

    Deck

---
+++ build/lib/CardRL/games/__init__.py
@@ -0,0 +1,3 @@
+__name__="texas"
+
+import texas
--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="CardRL",
-    version="0.0.1",
-    author="Vrishank Bhardwaj",
+    version="0.0.2",
+    author="Vrishank Bhardwaj, Vedant Chakravadhanula",
     author_email="vrishank1997@gmail.com",
     description="A Multi Agent Reinforcement Learning environment",
     long_description=long_description,
